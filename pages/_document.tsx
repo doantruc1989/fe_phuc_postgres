092d4129518f70aa492e20f62e44659b7507e06a
doantruc1989 <doantruc1989@gmail.com>
2023-07-06 10:03:58 +0700

    facebook plugin

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,11 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-      <link
+        <link
           rel="stylesheet"
           type="text/css"
           charSet="UTF-8"
@@ -17,9 +18,47 @@ export default function Document() {
         />
       </Head>
       <body>
+        <div id="fb-root"></div>
+
+        {/* <!-- Your Chat plugin code --> */}
+        <div id="fb-customer-chat" className="fb-customerchat"></div>
+        <Script
+          id="messenger-tag"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `<script>
+            var chatbox = document.getElementById('fb-customer-chat');
+            chatbox.setAttribute("page_id", "109721398444205");
+            chatbox.setAttribute("attribution", "biz_inbox");
+          </script>`,
+          }}
+        ></Script>
+        <Script
+          id="messenger-sdk"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `<script>
+            window.fbAsyncInit = function() {
+              FB.init({
+                xfbml            : true,
+                version          : 'v17.0'
+              });
+            };
+      
+            (function(d, s, id) {
+              var js, fjs = d.getElementsByTagName(s)[0];
+              if (d.getElementById(id)) return;
+              js = d.createElement(s); js.id = id;
+              js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+              fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));
+          </script>`,
+          }}
+        ></Script>
+
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
